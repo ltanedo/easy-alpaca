5c832e157f23cf44b39970d36706a53172019bd1
ltanedo <lloydtan@buffalo.edu>
2022-02-25 19:38:01 +0000

    repaired import

--- easy_alpaca/__endpoints__/setup.py
+++ easy_alpaca/__endpoints__/setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 setup(
-  name = 'easy_alpaca',         # How you named your package folder (MyLib)
-  packages = ['easy_alpaca','easy_alpaca.__endpoints__','easy_alpaca.__utility__'],   # Chose the same as "name"
+  name = 'easy_td',         # How you named your package folder (MyLib)
+  packages = ['easy_td','easy_td.__endpoints__','easy_td.__utlity__'],   # Chose the same as "name"
   version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A library for the Alpaca Trading and Data API',   # Give a short description about your library
+  description = 'A declarative library for making the TD Ameritrade API more accessible',   # Give a short description about your library
   author = 'lloydtan',                   # Type in your name
   author_email = 'lloydtan@buffalo.edu',      # Type in your E-Mail
-  url = 'https://github.com/ltanedo/easy-alpaca.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ltanedo/easy-alpaca/archive/refs/tags/v_02.tar.gz',    # I explain this later on
-  keywords = ['python', 'algo', 'trading', 'alpaca', 'brokerage', 'stocks', 'stock'],   # Keywords that define your package best
+  url = 'https://github.com/ltanedo/easy-td.git',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/ltanedo/easy-td/archive/refs/tags/v_02.tar.gz',    # I explain this later on
+  keywords = ['python', 'algo', 'trading', 'data', 'stocks', 'stock','td','ameritrade'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
   classifiers=[
